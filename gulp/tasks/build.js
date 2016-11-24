1a5b1edabd098da385338fbbf1bae309bb1e1cc9
rayGreycloud <rayGreycloud@gmail.com>
2016-11-24 09:35:33 -0800

    Add favicon

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -24,6 +24,7 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
   var pathsToCopy = [
     './app/**/*',
     '!./app/index.html',
+    '!./app/favicon.ico',
     '!./app/assets/images/**',
     '!./app/assets/styles/**',
     '!./app/assets/scripts/**',
